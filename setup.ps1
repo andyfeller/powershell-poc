a8aa5fa67d64e630680cce18993e3e2ad32a8dbb
Andy Feller <andyfeller@github.com>
2021-10-22 10:31:21 -0400

    Apparently -Force doesnt always work

--- setup.ps1
+++ setup.ps1
@@ -2,4 +2,4 @@
 $env:PSModulePath += "$([System.IO.Path]::PathSeparator)$($pwd.Path)"
 
 # Register Powershell repository; needed for publishing
-Register-PSRepository -Name "powershell-poc" -SourceLocation "https://pkgs.dev.azure.com/andyfeller/powershell-poc/_packaging/powershell-poc/nuget/v2" -PublishLocation "https://pkgs.dev.azure.com/andyfeller/powershell-poc/_packaging/powershell-poc/nuget/v2" -Force
+Register-PSRepository -Name "powershell-poc" -SourceLocation "https://pkgs.dev.azure.com/andyfeller/powershell-poc/_packaging/powershell-poc/nuget/v2" -PublishLocation "https://pkgs.dev.azure.com/andyfeller/powershell-poc/_packaging/powershell-poc/nuget/v2"
