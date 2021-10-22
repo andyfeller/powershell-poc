0eb485c51023ad56382c26c12abf3af43339a732
Andy Feller <andyfeller@github.com>
2021-10-22 13:44:51 -0400

    Improving setup script output

--- setup.ps1
+++ setup.ps1
@@ -1,5 +1,21 @@
 # Adding current path (workspace root) to PowerShell modules path; needed for testing and publishing
+Write-Output (@'
+Appending workspace to PS module path
+   Workspace: {0}
+   env:PSModulePath: {1}
+'@ -f $pwd.Path, $env:PSModulePath)
+
 $env:PSModulePath += "$([System.IO.Path]::PathSeparator)$($pwd.Path)"
 
+
 # Register Powershell repository; needed for publishing
-Register-PSRepository -Name "powershell-poc" -SourceLocation "https://pkgs.dev.azure.com/andyfeller/powershell-poc/_packaging/powershell-poc/nuget/v2" -PublishLocation "https://pkgs.dev.azure.com/andyfeller/powershell-poc/_packaging/powershell-poc/nuget/v2"
+$repositoryName = "poweshell-poc"
+$repositoryUri = "https://pkgs.dev.azure.com/andyfeller/powershell-poc/_packaging/powershell-poc/nuget/v2"
+
+Write-Output (@'
+Registering PS repository
+    Name: {0}
+    URI: {1}
+'@ -f $repositoryName, $repositoryUri)
+
+Register-PSRepository -Name $repositoryName -SourceLocation $repositoryUri -PublishLocation $repositoryUri
