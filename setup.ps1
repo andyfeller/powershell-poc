9b004c364a2f0702d8d850c878c62e680e7894ed
Andy Feller <andyfeller@github.com>
2021-11-02 17:53:03 -0400

    Refactor for script support

--- setup.ps1
+++ setup.ps1
@@ -1,8 +1,7 @@
 # Set variables based on optional env vars or defaults
-$modulePath = $env:REPO_MODULE_PATH ?? $pwd.Path
+$modulePath = "$PSScriptRoot/modules"
 $repositoryName = $env:REPOSITORY_NAME ?? "powershell-poc"
 $repositoryUri = $env:REPOSITORY_URI ?? "https://pkgs.dev.azure.com/andyfeller/powershell-poc/_packaging/powershell-poc/nuget/v2"
-
 
 # Adding current path (workspace root) to PowerShell modules path; needed for testing and publishing
 Write-Output (@'
