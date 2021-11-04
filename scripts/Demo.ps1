66c725d69eb319aea748b20c78aa1b45b1865a76
Andy Feller <andyfeller@github.com>
2021-11-04 10:43:42 -0400

    Adding new demo script

---
+++ scripts/Demo.ps1
@@ -0,0 +1,43 @@
+
+<#PSScriptInfo
+
+.VERSION 0.0.1
+
+.GUID 610226f6-aebc-4bc7-83ce-677984dde5ca
+
+.AUTHOR andyfeller@github.com
+
+.COMPANYNAME
+
+.COPYRIGHT
+
+.TAGS
+
+.LICENSEURI
+
+.PROJECTURI
+
+.ICONURI
+
+.EXTERNALMODULEDEPENDENCIES 
+
+.REQUIREDSCRIPTS
+
+.EXTERNALSCRIPTDEPENDENCIES
+
+.RELEASENOTES
+
+
+.PRIVATEDATA
+
+#>
+
+<# 
+
+.DESCRIPTION 
+ Simple script for demo purposes 
+
+#> 
+Param()
+
+Write-Output "This is a simple script for demo purposes."
